a5262bff2e9e9e64f15021e384421a35b090128e
Kris Payne <krispayne@me.com>
2023-12-09 12:44:43 -0600

    debug

--- scripts/pi-temps.py
+++ scripts/pi-temps.py
@@ -8,7 +8,9 @@ import glob
 import time
 import configparser
 import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+
 
 config = configparser.ConfigParser()
 config_dir = os.path.dirname(os.path.abspath(__file__))
