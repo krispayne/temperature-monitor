0a405cdc4375446ce2dbcbda3c4115dd9161aa0a
Kris Payne <krispayne@me.com>
2023-12-09 13:01:16 -0600

    clean

--- dashboard/router/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-    app.use('/', require('./routes/home'));
-    app.use('/sensors', require('./routes/sensors'));
-    app.use('/temps', require('./routes/temps'));
-}
